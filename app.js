565e27bdc890c74bec459b556a56bea02c1ecc36
Vladimir Grichina <vgrichina@gmail.com>
2010-06-05 03:27:00 +0300

    Displaying list of friends

--- app.js
+++ app.js
@@ -1,63 +1,92 @@
+// Configure modules paths
 require.paths.unshift("./express/lib");
 require.paths.unshift("./node-oauth/lib");
 require.paths.unshift("./express-auth/lib");
 
+// Use Express framework and its plugins
 require("express");
-
 require("express/plugins")
 use(Cookie)
 use(Session, { expires: (12).hours , reapInterval: (2).minutes })
 
+// Use secret values from separate module
+var secret = require("./secret");
+
+// Use Express authentication plugins, Twitter in particular
+Object.merge(global, require('express/plugins/auth'));
+var StrategyDefinition = require('express/plugins/strategyDefinition').StrategyDefinition;
+use(Auth, {
+        strategies: {
+            "twitter": new StrategyDefinition(Twitter, { consumerKey: secret.twitterConsumerKey, consumerSecret: secret.twitterConsumerSecret })
+        }
+});
+
+// Use system module, for debug output, etc.
 var sys = require("sys");
 
+// Use OAuth implementation classl
 var OAuth = require("oauth").OAuth;
-Object.merge(global, require('express/plugins/auth'));
 
-var twitterConsumerKey = "Am8JnxHXtZwrIuQnTlnQ";
-var twitterConsumerSecret = "5wuqO5WvAgyyGQSp0x5xnFkQH9V1AOOe2xy0fl7c7U";
+// Stores OAuth instance used to do queries to Twitter API
+var oauth =  new OAuth("http://twitter.com/oauth/request_token",
+                            "http://twitter.com/oauth/access_token",
+                            secret.twitterConsumerKey,
+                            secret.twitterConsumerSecret,
+                            "1.0",
+                            null,
+                            "HMAC-SHA1");
 
-var StrategyDefinition = require('express/plugins/strategyDefinition').StrategyDefinition;
-use(Auth, { 
-        strategies: {
-            "twitter": new StrategyDefinition(Twitter, { consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret })
-    }
-});
+
+function getResource(self, url, method, callback) {
+    oauth.getProtectedResource(url, method,
+        self.session.auth["twitter_oauth_token"], self.session.auth["twitter_oauth_token_secret"],
+        function (error, data) {
+            sys.puts('got protected resource ');
+            callback(data);
+        });
+}
 
 configure(function() {
     set("root", __dirname);
 });
 
+// Authenticate using Twitter OAuth
 get("/auth/twitter", function() {
     var self = this;
-    self.authenticate(['twitter'], function(error, authenticated) { 
+    self.authenticate(['twitter'], function(error, authenticated) {
         if (authenticated) {
-            var oa= new OAuth("http://twitter.com/oauth/request_token",
-                            "http://twitter.com/oauth/access_token",
-                            twitterConsumerKey,
-                            twitterConsumerSecret,
-                            "1.0",
-                            null,
-                            "HMAC-SHA1");
-            oa.getProtectedResource("http://twitter.com/statuses/user_timeline.xml", "GET",
-                self.session.auth["twitter_oauth_token"], self.session.auth["twitter_oauth_token_secret"],
-                function (error, data) {
-                    sys.p('got protected resource ')
-                    self.respond(200, "<html><h1>Hello! Twitter authenticated user ("+self.session.auth.user.username+")</h1>"+data+ "</html>")
-                });
+            // Authenticated successfully, redirect to main page
+            self.redirect("/");
         } else {
             sys.inspect(error);
-            self.respond(200, "<html><h1>Twitter authentication failed :( </h1></html>")
+            self.respond(200, "<html><h1>Twitter authentication failed :( </h1></html>");
         }
     });
 })
 
 get("/", function() {
-    this.render("following.html.haml", {
-        layout: false,
-        locals: {
-            following: [ {name: "foo"}, {name: "bar"} ]
-        } 
-    });
+    var self = this;
+
+    if (!self.isAuthenticated()) {
+        self.redirect("/auth/twitter");
+    } else {
+        getResource(self, "http://api.twitter.com/1/friends/ids.json", "GET", function (data) {
+            var friendsIds = eval(data);
+
+            sys.puts("friendsIds = ", friendsIds);
+
+            getResource(self, "http://api.twitter.com/1/users/lookup.json?user_id=" + friendsIds.join(","), "GET", function (data) {
+                var friends = eval(data);
+
+                self.render("following.html.haml", {
+                    layout: false,
+                    locals: {
+                        friends: friends
+                    }
+                });
+            });
+        });
+    }
 });
 
 run();
---
+++ secret.js
@@ -0,0 +1,3 @@
+
+exports.twitterConsumerKey = "Am8JnxHXtZwrIuQnTlnQ";
+exports.twitterConsumerSecret = "5wuqO5WvAgyyGQSp0x5xnFkQH9V1AOOe2xy0fl7c7U";
